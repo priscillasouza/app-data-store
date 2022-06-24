2744a1fe1a89a54ac0e2ee37d67e0ece14b52efd
Priscilla Souza <priscillasilva-24@hotmail.com>
2022-06-23 23:31:46 -0300

    ajustando layouts

--- app/src/main/java/com/example/datastore/MainActivity.kt
+++ app/src/main/java/com/example/datastore/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.datastore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.datastore.databinding.ActivityMainBinding
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarHome)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun show() {
+        val email: String
+        val password: String
+        runBlocking {
+            email = DataStoreManager.readEmailUserDataStore()
+            password = DataStoreManager.readPasswordUserDataStore()
+        }
+
+        Toast.makeText(this, "Nome: $email, Senha: $password", Toast.LENGTH_SHORT).show()
     }
 }
